319c070cc3d1f1189d1bc5135b683ee1da5e4511
alexandre.vuillermot <alexandre.vuillermot@kuehne-nagel.com>
2020-11-15 12:08:59 +0100

    with header & menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,28 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SlideMenuModule } from 'primeng/slidemenu';
+import { MenubarModule } from 'primeng/menubar';
+import { ButtonModule } from 'primeng/button';
+
+
+import { MenuComponent } from './menu/menu.component'
+import { HeaderComponent } from './header/header.component'
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    SlideMenuModule,
+    MenubarModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/menu/menu.component.ts
@@ -0,0 +1,86 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api'
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: [
+    './menu.component.css'
+  ]
+})
+export class MenuComponent implements OnInit {
+
+  items: MenuItem[];
+
+  constructor() {
+    this.items = new Array<MenuItem>();
+  }
+
+  ngOnInit(): void {
+    this.items = [
+      {
+        label: "Client",
+        icon: 'pi pi-fw pi-id-card',
+        items: [
+          {
+            label: "Créer un client"
+          },
+          {
+            label: "Mettre à jour client"
+          },
+          {
+            label: "Suivi client"
+          }
+        ]
+      },
+      {
+        label: "Articles",
+        icon: 'pi pi-fw pi-briefcase',
+        items: [
+          {
+            label: "Créer un article",
+          },
+          {
+            label: "Mise à jour article",
+          }
+        ]
+      },
+      {
+        label: "Devis",
+        icon: 'pi pi-fw pi-file',
+        items: [
+          {
+            label: "Créer"
+          },
+          {
+            label: "Suivre",
+          }
+        ]
+      },
+      {
+        label: "Bon de livraison",
+        icon: 'pi pi-fw pi-bookmark',
+        items: [
+          {
+            label: "Créer"
+          },
+          {
+            label: "Suivre",
+          }
+        ]
+      },
+      {
+        label: "Facturation",
+        icon: 'pi pi-fw pi-shopping-cart',
+        items: [
+          {
+            label: "Créer"
+          },
+          {
+            label: "Suivre",
+          }
+        ]
+      }
+    ];
+  }
+}
